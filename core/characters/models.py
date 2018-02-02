6a75eeb4a4b4237b579364567d565cc6d7184a4f
Tahvohck <Tahvohck@gmail.com>
2018-02-02 07:50:33 -0500

    Added more character data

--- core/characters/models.py
+++ core/characters/models.py
@@ -14,16 +14,22 @@ class Character(models.Model):
 		)
 
 	account = models.ForeignKey('accounts.Account', on_delete=models.CASCADE, related_name='characters',)
+	joined = models.DateTimeField(auto_now_add=True)
+	lastOn = models.DateTimeField(auto_now=True)
+
 	avatar = models.ImageField(default='accounts/0/files/avatar_null.png', upload_to=get_avatar_name)
 	title = models.CharField(max_length=24, blank=True, default='Citizen',)
-	name = models.CharField(max_length=24,)
-	biography = models.CharField(max_length=10 * 1024, blank=True, default='',)
-	shortDesc = models.CharField(max_length=256, blank=True, default='',)
+	name = models.CharField(max_length=24, default='John Doe')
+	age = models.PositiveIntegerField(default=7670)  # In days, roughly 21 years
+	height = models.PositiveSmallIntegerField(default=170)  # In cm
+
+	sex = models.CharField(max_length=8, default='')
 	race = models.CharField(max_length=24, blank=True, default='Human')
-	gender = models.CharField(max_length=8, default='')
 
 	def __str__(self):
 		return "{0.title} {0.name}".format(self).strip()
+	biography = models.CharField(max_length=10 * 1024, blank=True, default='',)
+	shortDesc = models.CharField(max_length=256, blank=True, default='A nondescript human specimen.',)
 
 	def __repr__(self):
 		return (
