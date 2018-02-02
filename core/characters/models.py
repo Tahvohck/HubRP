e5b69936d85b7d9a4824b2647f4dcd6882f6e507
Tahvohck <Tahvohck@gmail.com>
2018-02-01 22:26:03 -0500

    Initial character work

--- core/characters/models.py
+++ core/characters/models.py
@@ -2,3 +2,35 @@ from django.db import models
 
 
 # Create your models here.
+class Character(models.Model):
+	account = models.ForeignKey('accounts.Account', on_delete=models.CASCADE, related_name='characters',)
+	title = models.CharField(max_length=24, blank=True, default='Citizen',)
+	name = models.CharField(max_length=24,)
+	biography = models.CharField(max_length=10 * 1024, blank=True, default='',)
+	shortDesc = models.CharField(max_length=256, blank=True, default='',)
+	race = models.CharField(max_length=24, blank=True, default='Human')
+	gender = models.CharField(max_length=8, default='')
+
+	def __str__(self):
+		return "{0.title} {0.name}".format(self).strip()
+
+	def __repr__(self):
+		return (
+			"{0.title} {0.name}\n"
+			"{0.gender}{1}{0.race}\n"
+			"{0.shortDesc}".format(
+				self, "" if self.gender == str() else ' ')
+			).strip()
+
+
+class Stats(models.Model):
+	character = models.OneToOneField('Character', on_delete=models.CASCADE, related_name="stats+")
+	hitpoints = models.PositiveIntegerField(default=0)
+	hitpoints_max = models.PositiveIntegerField(default=0)
+	stamina = models.PositiveIntegerField(default=10000)  # Counted in hundreths of a stamina point
+	stamina_max = models.PositiveIntegerField(default=10000)  # Counted in hundreths of a stamina point
+	attack = models.PositiveIntegerField(default=0)
+	defense = models.PositiveIntegerField(default=0)
+	mana = models.PositiveIntegerField(default=0)
+
+	money = models.PositiveIntegerField(default=0)
--- rpsite/settings.py
+++ rpsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
 	'core.accounts',
+	'core.characters',
 	'core.locations',
     'django.contrib.admin',
     'django.contrib.auth',
