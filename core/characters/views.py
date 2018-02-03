02674c711a7ff1e46cc877909688ed02ed1d867f
Tahvohck <Tahvohck@gmail.com>
2018-02-03 09:47:12 -0500

    Unified core charCard logic, made age a moving target

--- core/characters/views.py
+++ core/characters/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.timezone import now
 from .models import Character
 
 
 # Create your views here.
-def charCardByCID(request, cid, **kwargs):
-	character = get_object_or_404(Character, pk=cid)
+def charCard(request, character, **kwargs):
+	'''Given a character, calculate their age now, then render a character card'''
+	ageNow = (now() - character.joined).days + character.age
 	return render(request, 'characters/char-card.html', context={
 		'character': character,
-	})
-
-def charCardByName(request, name, **kwargs):
-	character = get_object_or_404(Character, name=name)
-	return render(request, 'characters/char-card.html', context={
-		'character': character,
+		'calcAge': ageNow,
 		'css': True, 'noCaptions': False,
 	})
+
+def charCardByCID(request, cid, **kwargs):
+	'''Get character by CID then pass to `charCard()`'''
+	character = get_object_or_404(Character, pk=cid)
+	return charCard(request, character, **kwargs)
+
+def charCardByName(request, name, **kwargs):
+	'''Get character by name then pass to `charCard()`'''
+	character = get_object_or_404(Character, name=name)
+	return charCard(request, character, **kwargs)
