edbe9cbbadc57451d177cb62fc3c5d339b53b515
Tahvohck <Tahvohck@gmail.com>
2018-02-03 13:47:53 -0500

    PEP8

--- core/characters/views.py
+++ core/characters/views.py
@@ -13,10 +13,12 @@ def charCard(request, character, **kwargs):
 		'css': True, 'noCaptions': False,
 	})
 
+
 def charCardByCID(request, cid, **kwargs):
 	'''Get character by CID then pass to `charCard()`'''
 	character = get_object_or_404(Character, pk=cid)
 	return charCard(request, character, **kwargs)
+
 
 def charCardByName(request, name, **kwargs):
 	'''Get character by name then pass to `charCard()`'''
