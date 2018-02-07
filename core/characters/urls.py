c02b1ed2d4493f128180e518fb25f8c4cde0ab3d
Tahvohck <Tahvohck@gmail.com>
2018-02-07 02:13:21 -0500

    Unique URL name

--- core/characters/urls.py
+++ core/characters/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 app_name = Config.name
 urlpatterns = [
-	path('cards',			vCharCards,			name='char-card'),
+	path('cards',			vCharCards,			name='char-card-index'),
 	path('card/<int:cid>',	vCharCardByCID,		name='char-card'),
 	path('card/<str:name>',	vCharCardByName,	name='char-card'),
 ]
