a83c81f7331f4cc289a037dff5afab07c6b07bad
Tahvohck <Tahvohck@gmail.com>
2018-02-07 03:52:04 -0500

    BUGFIX: Removed duplicate url definitions between char-card and char-detail

--- core/characters/urls.py
+++ core/characters/urls.py
@@ -5,9 +5,9 @@ from .views import *
 
 app_name = Config.name
 urlpatterns = [
+	path('<int:cid>',		vCharByCID,			name='char-detail'),
+	path('<str:name>',		vCharByName,		name='char-detail'),
 	path('cards',			vCharCards,			name='char-card-index'),
 	path('card/<int:cid>',	vCharCardByCID,		name='char-card'),
 	path('card/<str:name>',	vCharCardByName,	name='char-card'),
-	path('card/<int:cid>',	vCharCardByName,	name='char-detail'),
-	path('card/<str:name>',	vCharCardByName,	name='char-detail'),
 ]
