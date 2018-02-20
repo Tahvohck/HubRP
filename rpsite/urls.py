27148a31e8d57d5422e7f88c206836ef7b591482
Tahvohck <Tahvohck@gmail.com>
2018-02-20 08:48:35 -0500

    re-enabled accounts as profile

--- rpsite/urls.py
+++ rpsite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('profile', include('core.accounts.urls')),
     path('legacy/accounts/', include('core.accounts.urls')),
     path('legacy/char/', include('core.characters.urls')),
     path("", include('core.pages.urls')),
