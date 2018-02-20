34c41351b849680c380e9228699cdfe5a4b57b0c
Tahvohck <Tahvohck@gmail.com>
2018-02-20 01:20:05 -0500

    Preparing for restructure

--- rpsite/urls.py
+++ rpsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('core.accounts.urls')),
-    path('char/', include('core.characters.urls')),
+    path('legacy/accounts/', include('core.accounts.urls')),
+    path('legacy/char/', include('core.characters.urls')),
 ]
