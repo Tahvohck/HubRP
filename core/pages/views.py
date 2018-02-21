d755eedc1326282236b5fcbe133a9a945945a251
Tahvohck <Tahvohck@gmail.com>
2018-02-20 21:42:56 -0500

    Add registration code

--- core/pages/views.py
+++ core/pages/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import *
@@ -22,12 +22,23 @@ def homepage(request):
 		form = f_Login(request.POST)
 		username = request.POST['username']
 		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect(_profile)
-		else:
-			form.errors['username'] = ['You did not specify a valid username/password.']
+		if request.POST['submit'] == 'login':
+			# User is logging in. Try to authenticate and log in.
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect(_profile)
+			else:
+				form.errors['username'] = ['You did not specify a valid username/password.']
+		elif request.POST['submit'] == 'register':
+			# New user is trying to register
+			User = get_user_model()
+			if User.objects.filter(username=username).exists():
+				form.errors['username'] = ['User already exists']
+			else:
+				newuser = User.objects.create_user(username, password=password)
+				login(request, newuser)
+				return redirect(_profile)
 
 	return render(request, 'core/homepage.html', context=dict(
 		sitename='Twisted Hub: home page', form=form
